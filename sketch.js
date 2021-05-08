cb53702d31aaf90814a00e1cdb6524b8d9916647
Naren-dandu <75018973+Naren-dandu@users.noreply.github.com>
2021-05-08 12:06:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var database,canvas,backgroungImage;
+var gameState = 0;
+var playerCount;
+var form,player,game;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(400,400);
+game = new Game();
+game.getState();
+game.start();
+
+ 
+}
+
+function draw(){
+  
+  
+}
+
